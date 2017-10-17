fa138df9bf086caf58e2dbec842972c357d17cb5
fionawolfe <32306971+fionawolfe@users.noreply.github.com>
2017-10-17 11:12:49 +0100

    Add files via upload

--- LowestCommonAncestorTest.java
+++ LowestCommonAncestorTest.java
@@ -3,22 +3,24 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LowestCommonAncestorTest {
-	
+
 	@Test
 	public void testSizeEmpty() {
 		LowestCommonAncestor binaryTreeEmpty = new LowestCommonAncestor();
+
 		assertEquals("Lowest common ancestor of empty tree", -1, binaryTreeEmpty.LowestCommonAncestor(0, 0));
 	}
-	
+
 	@Test
 	public void testSizeOneNode() {
 		LowestCommonAncestor binaryTreeOneNode = new LowestCommonAncestor();
 		binaryTreeOneNode.rootNode = new Node(1);
+
 		assertEquals("Lowest common ancestor of one node tree", -1, binaryTreeOneNode.LowestCommonAncestor(1, 0));
 	}
-	
+
 	LowestCommonAncestor binaryTree15Nodes = new LowestCommonAncestor();
-	
+
 	@Test
 	public void testSize15Nodes() {
 		binaryTree15Nodes.rootNode = new Node(1);
@@ -36,7 +38,7 @@ public class LowestCommonAncestorTest {
 		binaryTree15Nodes.rootNode.right.left.right = new Node(13);
 		binaryTree15Nodes.rootNode.right.right.left = new Node(14);
 		binaryTree15Nodes.rootNode.right.right.right = new Node(15);
-		
+
 		assertEquals("Lowest common ancestor of 15 node tree", 1, binaryTree15Nodes.LowestCommonAncestor(1, 2));
 		assertEquals("Lowest common ancestor of 15 node tree", 1, binaryTree15Nodes.LowestCommonAncestor(2, 3));
 		assertEquals("Lowest common ancestor of 15 node tree", 2, binaryTree15Nodes.LowestCommonAncestor(2, 4));
@@ -51,10 +53,10 @@ public class LowestCommonAncestorTest {
 		assertEquals("Lowest common ancestor of 15 node tree", 3, binaryTree15Nodes.LowestCommonAncestor(13, 14));
 		assertEquals("Lowest common ancestor of 15 node tree", 1, binaryTree15Nodes.LowestCommonAncestor(15, 1));
 	}
-	
+
 	@Test
-	public void testNonExistentNode()
-	{
+	public void testNonExistentNode() {
+		LowestCommonAncestor binaryTree15Nodes = new LowestCommonAncestor();
 		binaryTree15Nodes.rootNode = new Node(1);
 		binaryTree15Nodes.rootNode.left = new Node(2);
 		binaryTree15Nodes.rootNode.right = new Node(3);
@@ -70,9 +72,25 @@ public class LowestCommonAncestorTest {
 		binaryTree15Nodes.rootNode.right.left.right = new Node(13);
 		binaryTree15Nodes.rootNode.right.right.left = new Node(14);
 		binaryTree15Nodes.rootNode.right.right.right = new Node(15);
-		
-		assertEquals("Testing non existent node", -1, binaryTree15Nodes.LowestCommonAncestor(2, 17));
-		assertEquals("Testing non existent node", -1, binaryTree15Nodes.LowestCommonAncestor(23, 17));
-		
+
+		assertEquals(-1, binaryTree15Nodes.LowestCommonAncestor(2, 17));
+		assertEquals(-1, binaryTree15Nodes.LowestCommonAncestor(23, 17));
+	}
+
+	@Test
+	public void outOfOrderTest() {
+		LowestCommonAncestor binaryTreeOutOfOrder = new LowestCommonAncestor();
+		binaryTreeOutOfOrder.rootNode = new Node(5);
+		binaryTreeOutOfOrder.rootNode.left = new Node(3);
+		binaryTreeOutOfOrder.rootNode.right = new Node(1);
+		binaryTreeOutOfOrder.rootNode.left.left = new Node(4);
+		binaryTreeOutOfOrder.rootNode.left.right = new Node(7);
+		binaryTreeOutOfOrder.rootNode.right.left = new Node(2);
+		binaryTreeOutOfOrder.rootNode.right.right = new Node(6);
+
+		assertEquals("Lowest common ancestor of tree out of order", 5, binaryTreeOutOfOrder.LowestCommonAncestor(6, 4));
+		assertEquals("Lowest common ancestor of tree out of order", 3, binaryTreeOutOfOrder.LowestCommonAncestor(3, 4));
+		assertEquals("Lowest common ancestor of tree out of order", 1, binaryTreeOutOfOrder.LowestCommonAncestor(6, 2));
+		assertEquals("Lowest common ancestor of tree out of order", 5, binaryTreeOutOfOrder.LowestCommonAncestor(7, 2));
 	}
 }
