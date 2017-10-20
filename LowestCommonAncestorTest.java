e2e649effb231d13b6f6e1718d2559dd1322f76b
fionawolfe <32306971+fionawolfe@users.noreply.github.com>
2017-10-20 15:23:58 +0100

    Add files via upload
    Modified name of Lowest Common Ancestor function.

--- LowestCommonAncestorTest.java
+++ LowestCommonAncestorTest.java
@@ -8,7 +8,7 @@ public class LowestCommonAncestorTest {
 	public void testSizeEmpty() {
 		LowestCommonAncestor binaryTreeEmpty = new LowestCommonAncestor();
 
-		assertEquals("Lowest common ancestor of empty tree", -1, binaryTreeEmpty.LowestCommonAncestor(0, 0));
+		assertEquals("Lowest common ancestor of empty tree", -1, binaryTreeEmpty.LowestCommonAncestorFunction(0, 0));
 	}
 
 	@Test
@@ -16,7 +16,7 @@ public class LowestCommonAncestorTest {
 		LowestCommonAncestor binaryTreeOneNode = new LowestCommonAncestor();
 		binaryTreeOneNode.rootNode = new Node(1);
 
-		assertEquals("Lowest common ancestor of one node tree", -1, binaryTreeOneNode.LowestCommonAncestor(1, 0));
+		assertEquals("Lowest common ancestor of one node tree", -1, binaryTreeOneNode.LowestCommonAncestorFunction(1, 0));
 	}
 
 	LowestCommonAncestor binaryTree15Nodes = new LowestCommonAncestor();
@@ -39,19 +39,19 @@ public class LowestCommonAncestorTest {
 		binaryTree15Nodes.rootNode.right.right.left = new Node(14);
 		binaryTree15Nodes.rootNode.right.right.right = new Node(15);
 
-		assertEquals("Lowest common ancestor of 15 node tree", 1, binaryTree15Nodes.LowestCommonAncestor(1, 2));
-		assertEquals("Lowest common ancestor of 15 node tree", 1, binaryTree15Nodes.LowestCommonAncestor(2, 3));
-		assertEquals("Lowest common ancestor of 15 node tree", 2, binaryTree15Nodes.LowestCommonAncestor(2, 4));
-		assertEquals("Lowest common ancestor of 15 node tree", 2, binaryTree15Nodes.LowestCommonAncestor(4, 5));
-		assertEquals("Lowest common ancestor of 15 node tree", 3, binaryTree15Nodes.LowestCommonAncestor(6, 7));
-		assertEquals("Lowest common ancestor of 15 node tree", 4, binaryTree15Nodes.LowestCommonAncestor(4, 8));
-		assertEquals("Lowest common ancestor of 15 node tree", 4, binaryTree15Nodes.LowestCommonAncestor(8, 9));
-		assertEquals("Lowest common ancestor of 15 node tree", 6, binaryTree15Nodes.LowestCommonAncestor(6, 12));
-		assertEquals("Lowest common ancestor of 15 node tree", 7, binaryTree15Nodes.LowestCommonAncestor(14, 15));
-		assertEquals("Lowest common ancestor of 15 node tree", 2, binaryTree15Nodes.LowestCommonAncestor(8, 2));
-		assertEquals("Lowest common ancestor of 15 node tree", 2, binaryTree15Nodes.LowestCommonAncestor(9, 11));
-		assertEquals("Lowest common ancestor of 15 node tree", 3, binaryTree15Nodes.LowestCommonAncestor(13, 14));
-		assertEquals("Lowest common ancestor of 15 node tree", 1, binaryTree15Nodes.LowestCommonAncestor(15, 1));
+		assertEquals("Lowest common ancestor of 15 node tree", 1, binaryTree15Nodes.LowestCommonAncestorFunction(1, 2));
+		assertEquals("Lowest common ancestor of 15 node tree", 1, binaryTree15Nodes.LowestCommonAncestorFunction(2, 3));
+		assertEquals("Lowest common ancestor of 15 node tree", 2, binaryTree15Nodes.LowestCommonAncestorFunction(2, 4));
+		assertEquals("Lowest common ancestor of 15 node tree", 2, binaryTree15Nodes.LowestCommonAncestorFunction(4, 5));
+		assertEquals("Lowest common ancestor of 15 node tree", 3, binaryTree15Nodes.LowestCommonAncestorFunction(6, 7));
+		assertEquals("Lowest common ancestor of 15 node tree", 4, binaryTree15Nodes.LowestCommonAncestorFunction(4, 8));
+		assertEquals("Lowest common ancestor of 15 node tree", 4, binaryTree15Nodes.LowestCommonAncestorFunction(8, 9));
+		assertEquals("Lowest common ancestor of 15 node tree", 6, binaryTree15Nodes.LowestCommonAncestorFunction(6, 12));
+		assertEquals("Lowest common ancestor of 15 node tree", 7, binaryTree15Nodes.LowestCommonAncestorFunction(14, 15));
+		assertEquals("Lowest common ancestor of 15 node tree", 2, binaryTree15Nodes.LowestCommonAncestorFunction(8, 2));
+		assertEquals("Lowest common ancestor of 15 node tree", 2, binaryTree15Nodes.LowestCommonAncestorFunction(9, 11));
+		assertEquals("Lowest common ancestor of 15 node tree", 3, binaryTree15Nodes.LowestCommonAncestorFunction(13, 14));
+		assertEquals("Lowest common ancestor of 15 node tree", 1, binaryTree15Nodes.LowestCommonAncestorFunction(15, 1));
 	}
 
 	@Test
@@ -73,8 +73,8 @@ public class LowestCommonAncestorTest {
 		binaryTree15Nodes.rootNode.right.right.left = new Node(14);
 		binaryTree15Nodes.rootNode.right.right.right = new Node(15);
 
-		assertEquals(-1, binaryTree15Nodes.LowestCommonAncestor(2, 17));
-		assertEquals(-1, binaryTree15Nodes.LowestCommonAncestor(23, 17));
+		assertEquals(-1, binaryTree15Nodes.LowestCommonAncestorFunction(2, 17));
+		assertEquals(-1, binaryTree15Nodes.LowestCommonAncestorFunction(23, 17));
 	}
 
 	@Test
@@ -88,9 +88,9 @@ public class LowestCommonAncestorTest {
 		binaryTreeOutOfOrder.rootNode.right.left = new Node(2);
 		binaryTreeOutOfOrder.rootNode.right.right = new Node(6);
 
-		assertEquals("Lowest common ancestor of tree out of order", 5, binaryTreeOutOfOrder.LowestCommonAncestor(6, 4));
-		assertEquals("Lowest common ancestor of tree out of order", 3, binaryTreeOutOfOrder.LowestCommonAncestor(3, 4));
-		assertEquals("Lowest common ancestor of tree out of order", 1, binaryTreeOutOfOrder.LowestCommonAncestor(6, 2));
-		assertEquals("Lowest common ancestor of tree out of order", 5, binaryTreeOutOfOrder.LowestCommonAncestor(7, 2));
+		assertEquals("Lowest common ancestor of tree out of order", 5, binaryTreeOutOfOrder.LowestCommonAncestorFunction(6, 4));
+		assertEquals("Lowest common ancestor of tree out of order", 3, binaryTreeOutOfOrder.LowestCommonAncestorFunction(3, 4));
+		assertEquals("Lowest common ancestor of tree out of order", 1, binaryTreeOutOfOrder.LowestCommonAncestorFunction(6, 2));
+		assertEquals("Lowest common ancestor of tree out of order", 5, binaryTreeOutOfOrder.LowestCommonAncestorFunction(7, 2));
 	}
 }
