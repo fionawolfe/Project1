77533ff0d03213aa5a7fe281c5df01121e0454d9
fionawolfe <32306971+fionawolfe@users.noreply.github.com>
2017-10-30 16:09:46 +0000

    Added in binary tree tests

--- DirectedAcyclicGraphTest.java
+++ DirectedAcyclicGraphTest.java
@@ -6,6 +6,95 @@ import org.junit.Test;
 
 public class DirectedAcyclicGraphTest {
 
+	@Test
+	public void testSizeEmpty() {
+		DirectedAcyclicGraph binaryTreeEmpty = new DirectedAcyclicGraph();
+
+		assertEquals("Lowest common ancestor of empty tree", -1, binaryTreeEmpty.LowestCommonAncestorFunctionBinaryTree(0, 0));
+	}
+
+	@Test
+	public void testSizeOneNode() {
+		DirectedAcyclicGraph binaryTreeOneNode = new DirectedAcyclicGraph();
+		binaryTreeOneNode.rootNode = new Node(1);
+
+		assertEquals("Lowest common ancestor of one node tree", -1, binaryTreeOneNode.LowestCommonAncestorFunctionBinaryTree(1, 0));
+	}
+
+	DirectedAcyclicGraph binaryTree15Nodes = new DirectedAcyclicGraph();
+
+	@Test
+	public void testSize15Nodes() {
+		binaryTree15Nodes.rootNode = new Node(1);
+		binaryTree15Nodes.rootNode.left = new Node(2);
+		binaryTree15Nodes.rootNode.right = new Node(3);
+		binaryTree15Nodes.rootNode.left.left = new Node(4);
+		binaryTree15Nodes.rootNode.left.right = new Node(5);
+		binaryTree15Nodes.rootNode.right.left = new Node(6);
+		binaryTree15Nodes.rootNode.right.right = new Node(7);
+		binaryTree15Nodes.rootNode.left.left.left = new Node(8);
+		binaryTree15Nodes.rootNode.left.left.right = new Node(9);
+		binaryTree15Nodes.rootNode.left.right.left = new Node(10);
+		binaryTree15Nodes.rootNode.left.right.right = new Node(11);
+		binaryTree15Nodes.rootNode.right.left.left = new Node(12);
+		binaryTree15Nodes.rootNode.right.left.right = new Node(13);
+		binaryTree15Nodes.rootNode.right.right.left = new Node(14);
+		binaryTree15Nodes.rootNode.right.right.right = new Node(15);
+
+		assertEquals("Lowest common ancestor of 15 node tree", 1, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(1, 2));
+		assertEquals("Lowest common ancestor of 15 node tree", 1, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(2, 3));
+		assertEquals("Lowest common ancestor of 15 node tree", 2, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(2, 4));
+		assertEquals("Lowest common ancestor of 15 node tree", 2, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(4, 5));
+		assertEquals("Lowest common ancestor of 15 node tree", 3, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(6, 7));
+		assertEquals("Lowest common ancestor of 15 node tree", 4, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(4, 8));
+		assertEquals("Lowest common ancestor of 15 node tree", 4, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(8, 9));
+		assertEquals("Lowest common ancestor of 15 node tree", 6, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(6, 12));
+		assertEquals("Lowest common ancestor of 15 node tree", 7, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(14, 15));
+		assertEquals("Lowest common ancestor of 15 node tree", 2, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(8, 2));
+		assertEquals("Lowest common ancestor of 15 node tree", 2, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(9, 11));
+		assertEquals("Lowest common ancestor of 15 node tree", 3, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(13, 14));
+		assertEquals("Lowest common ancestor of 15 node tree", 1, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(15, 1));
+	}
+
+	@Test
+	public void testNonExistentNode() {
+		DirectedAcyclicGraph binaryTree15Nodes = new DirectedAcyclicGraph();
+		binaryTree15Nodes.rootNode = new Node(1);
+		binaryTree15Nodes.rootNode.left = new Node(2);
+		binaryTree15Nodes.rootNode.right = new Node(3);
+		binaryTree15Nodes.rootNode.left.left = new Node(4);
+		binaryTree15Nodes.rootNode.left.right = new Node(5);
+		binaryTree15Nodes.rootNode.right.left = new Node(6);
+		binaryTree15Nodes.rootNode.right.right = new Node(7);
+		binaryTree15Nodes.rootNode.left.left.left = new Node(8);
+		binaryTree15Nodes.rootNode.left.left.right = new Node(9);
+		binaryTree15Nodes.rootNode.left.right.left = new Node(10);
+		binaryTree15Nodes.rootNode.left.right.right = new Node(11);
+		binaryTree15Nodes.rootNode.right.left.left = new Node(12);
+		binaryTree15Nodes.rootNode.right.left.right = new Node(13);
+		binaryTree15Nodes.rootNode.right.right.left = new Node(14);
+		binaryTree15Nodes.rootNode.right.right.right = new Node(15);
+
+		assertEquals(-1, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(2, 17));
+		assertEquals(-1, binaryTree15Nodes.LowestCommonAncestorFunctionBinaryTree(23, 17));
+	}
+
+	@Test
+	public void outOfOrderTest() {
+		DirectedAcyclicGraph binaryTreeOutOfOrder = new DirectedAcyclicGraph();
+		binaryTreeOutOfOrder.rootNode = new Node(5);
+		binaryTreeOutOfOrder.rootNode.left = new Node(3);
+		binaryTreeOutOfOrder.rootNode.right = new Node(1);
+		binaryTreeOutOfOrder.rootNode.left.left = new Node(4);
+		binaryTreeOutOfOrder.rootNode.left.right = new Node(7);
+		binaryTreeOutOfOrder.rootNode.right.left = new Node(2);
+		binaryTreeOutOfOrder.rootNode.right.right = new Node(6);
+
+		assertEquals("Lowest common ancestor of tree out of order", 5, binaryTreeOutOfOrder.LowestCommonAncestorFunctionBinaryTree(6, 4));
+		assertEquals("Lowest common ancestor of tree out of order", 3, binaryTreeOutOfOrder.LowestCommonAncestorFunctionBinaryTree(3, 4));
+		assertEquals("Lowest common ancestor of tree out of order", 1, binaryTreeOutOfOrder.LowestCommonAncestorFunctionBinaryTree(6, 2));
+		assertEquals("Lowest common ancestor of tree out of order", 5, binaryTreeOutOfOrder.LowestCommonAncestorFunctionBinaryTree(7, 2));
+	}
 @Test
 public void testDAG1 ()
 {
