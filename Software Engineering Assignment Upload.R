b6c66312d5f92f76363fdac7f7fc837b495fd42a
fionawolfe <32306971+fionawolfe@users.noreply.github.com>
2017-12-11 16:43:58 +0000

    Added Github token

--- Software Engineering Assignment Upload.R
+++ Software Engineering Assignment Upload.R
@@ -13,5 +13,6 @@ GithubApp <- oauth_app(appname = "Software Engineering Assignment",
                        key = "908906573ab482b799db",
                        secret = "63be0fe2a7af04af714d3634f86cbfbc8121e036")
 
-
+github_token1 <- oauth2.0_token(oauth_endpoints("github"), GithubApp)
+myToken <- config(token = github_token1)
 
