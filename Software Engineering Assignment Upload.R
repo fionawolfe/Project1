0cfdee053860d65a41cbc180c3b0d83f64ec56b6
fionawolfe <32306971+fionawolfe@users.noreply.github.com>
2017-12-12 12:39:22 +0000

    Remove duplicates from the list

--- Software Engineering Assignment Upload.R
+++ Software Engineering Assignment Upload.R
@@ -10,8 +10,8 @@ oauth_endpoints("github")
 
 
 GithubApp = oauth_app(appname = "Software Engineering Assignment",
-                       key = "908906573ab482b799db",
-                       secret = "63be0fe2a7af04af714d3634f86cbfbc8121e036")
+                      key = "908906573ab482b799db",
+                      secret = "63be0fe2a7af04af714d3634f86cbfbc8121e036")
 
 github_token1 = oauth2.0_token(oauth_endpoints("github"), GithubApp)
 myToken = config(token = github_token1)
@@ -26,7 +26,10 @@ for (i in 1:length(UserIds))
   id2 = data2$login
   for (j in 1:length(id2))
   {
-  UserIds[length(UserIds) +1] = id2[j]
+    if(is.element(id2[j], UserIds) == FALSE)
+    {
+      UserIds[length(UserIds) +1] = id2[j]
+    }
   }
 }
 
