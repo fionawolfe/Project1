a17e25a0f2544ef736d638651829cb8b1a34b29d
fionawolfe <32306971+fionawolfe@users.noreply.github.com>
2017-12-12 13:50:18 +0000

    Edited Github access method

--- Software Engineering Assignment Upload.R
+++ Software Engineering Assignment Upload.R
@@ -16,22 +16,29 @@ GithubApp = oauth_app(appname = "Software Engineering Assignment",
 github_token1 = oauth2.0_token(oauth_endpoints("github"), GithubApp)
 myToken = config(token = github_token1)
 
-data1 = fromJSON("https://api.github.com/users/fionawolfe/followers")
-id = data1$login
+data1 = GET("https://api.github.com/users/fionawolfe/followers", myToken)
+content1 = content(data1)
+followersDF1 = jsonlite::fromJSON(jsonlite::toJSON(content1))
+id = followersDF1$login
 UserIds = c(id)
 AllUsers = c()
 for (i in 1:length(UserIds))
 {
-  data2 = fromJSON(paste0("https://api.github.com/users/", UserIds[i], "/followers"))
-  id2 = data2$login
+  url2 = paste("https://api.github.com/users/", UserIds[i], "/followers", sep = "")
+  data2 = GET(url, myToken)
+  content2 = content(data2)
+  followersDF2 = jsonlite::fromJSON(jsonlite::toJSON(content2))
+  id2 = followersDF2$login
   for (j in 1:length(id2))
   {
     if(is.element(id2[j], UserIds) == FALSE)
     {
       UserIds[length(UserIds) +1] = id2[j]
     }
+    next
   }
+  next
 }
-
+UserIds
 
 
