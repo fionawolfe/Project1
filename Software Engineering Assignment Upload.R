d21488ebcee06da00667029798f48ac3e1f13302
fionawolfe <32306971+fionawolfe@users.noreply.github.com>
2017-12-14 11:19:54 +0000

    Added number of repositories to data frame

--- Software Engineering Assignment Upload.R
+++ Software Engineering Assignment Upload.R
@@ -22,7 +22,7 @@ followersDF1 = jsonlite::fromJSON(jsonlite::toJSON(content1))
 id = followersDF1$login
 UserIds = c(id)
 AllUsers = c()
-AllUsersDF = data.frame(Username = integer(), Followers = integer(), Following = integer())
+AllUsersDF = data.frame(Username = integer(), Followers = integer(), Following = integer(), NumberOfRepositories = integer())
 
 
 for (i in 1:length(UserIds))
@@ -45,8 +45,9 @@ for (i in 1:length(UserIds))
       followersDF3 = jsonlite::fromJSON(jsonlite::toJSON(content3))
       followers = followersDF3$followers
       following = followersDF3$following
+      reposnumber = followersDF3$public_repos
       AllUsers[length(AllUsers) +1] = usernames[j]
-      AllUsersDF[nrow(AllUsersDF)+1,] = c(usernames[j], followers, following)
+      AllUsersDF[nrow(AllUsersDF)+1,] = c(usernames[j], followers, following, reposnumber)
       
     }
     next
